173f7932a204a7565e8f956c1ffcdfd01479925f
huya.nzb <huya.nzb@taobao.com>
2019-09-18 16:09:30 +0800

    finish

---
+++ index.js
@@ -0,0 +1,32 @@
+const values = `
+// Object.values polyfill
+if (!Object.values) {
+  Object.values = function (target) {
+    return Object.keys(target).map(function (key) {
+      return target[key];
+    });
+  }
+}
+`;
+
+const entries = `
+// Object.entries polyfill
+if (!Object.entries) {
+  Object.entries = function (target) {
+    return Object.keys(target).map(function (key) {
+      return [key, target[key]];
+    });
+  }
+}
+`;
+
+const polyfill = ';(function(){' + values + entries + '})();\n\n';
+
+module.exports = function(webpack, opts) {
+  webpack = webpack || require('webpack');
+  return new webpack.BannerPlugin(Object.assign({
+    banner: polyfill,
+    raw: true, 
+    entryOnly: true
+  }, opts));
+}
